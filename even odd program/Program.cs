3c1e514108a148cf03e6d76e473bc57878b1e0a2
komal <komal@LAPTOP-GPSG7DIK>
2019-05-21 10:29:47 +1200

    Add project files.

---
+++ even odd program/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd_program
+{
+    class Program
+    {
+        //This program will find the number is even or add
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any Number = ");
+            int num = int.Parse(Console.ReadLine());
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("Number is even number");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd number");
+            }
+        }
+    }
+}
